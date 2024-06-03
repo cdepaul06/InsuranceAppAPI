b4ff56a56341d4eca5898a7d1db8ac55b6a2ad6e
Chris <73894631+cdepaul06@users.noreply.github.com>
2024-06-02 22:15:23 -0400

    Update models
    Update customer to have Add1 + Add2

---
+++ InsuranceAppAPI/Migrations/20240603011436_ChangeCustomerTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsuranceAppAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCustomerTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Customers",
+                newName: "Address1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address2",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address2",
+                table: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "Address1",
+                table: "Customers",
+                newName: "Address");
+        }
+    }
+}
--- InsuranceAppAPI/Models/Customer.cs
+++ InsuranceAppAPI/Models/Customer.cs
@@ -13,7 +13,8 @@ namespace InsuranceAppAPI.Models
         [MaxLength(50)]
         public required string Email { get; set; }
         public required string Phone { get; set; }
-        public required string Address { get; set; }
+        public required string Address1 { get; set; }
+        public string? Address2 { get; set; }
         public required string City { get; set; }
         public required string State { get; set; }
         public required int Zip { get; set; }
